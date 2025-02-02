5b3c23578f8c129e45599ee30de8e0e773c077b3
Kaosi Anikwe <anikwehenryasa@gmail.com>
2025-02-02 18:44:10 +0100

    slots and reservations

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -9,6 +9,9 @@ from flask import Blueprint, request, current_app, jsonify
 from .. import logger
 from ..models import Dish, Order
 
+from datetime import datetime, timedelta
+from sqlalchemy.exc import SQLAlchemyError
+
 api = Blueprint("api", __name__)
 
 #Dishes
@@ -103,3 +106,59 @@ def get_dishes():
     except Exception as e:
         logger.error(traceback.format_exc())
         return jsonify({"message": "Error retrieving dishes", "error": str(e)}), 500
+    
+def available_tables():
+    try:
+        # Locking the table records to prevent race condition using SELECT FOR UPDATE
+        with db.session.begin():
+            available_table = db.session.execute(
+                "SELECT table_id FROM orders WHERE table_id BETWEEN 1 AND 30 FOR UPDATE SKIP LOCKED"
+            ).fetchone()
+
+            if available_table:
+                return available_table.table_id
+            return None
+    except SQLAlchemyError as e:
+        db.session.rollback()  # Rollback in case of error
+        raise Exception("Database error: " + str(e))
+
+@api.post("/reservations")
+def reservations():
+    try:
+        data = request.get_json()
+
+        duration = data.get("duration")  
+        day = data.get("day")  
+        time = data.get("time") 
+
+        table_id = available_tables()
+        if table_id is None:
+            return jsonify({"message": "No tables available."}), 400
+        
+        start_time_str = f"{day} {time}"
+        start_time = datetime.strptime(start_time_str, "%Y-%m-%d %H:%M")
+
+        # Duration is in minutes
+        end_time = start_time + timedelta(minutes=duration)
+
+        # Check for reservation overlaps
+        existing_reservations = Order.get_all(table_id=table_id)
+        for reservation in existing_reservations:
+            if (start_time < reservation.end_time and end_time > reservation.start_time):
+                return jsonify({"message": "The slot is already reserved."}), 400
+
+        taken_slot = Order(
+            table_id=table_id,
+            start_time=start_time,
+            end_time=end_time,
+            duration=duration,
+            day=day,
+            time=time
+        )
+        taken_slot.insert()
+
+        return jsonify({"message": "Reservation successful", "table_id": table_id, "start_time": start_time, "end_time": end_time})
+
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        return jsonify({"message": "Error reserving slot", "error": str(e)}), 500
